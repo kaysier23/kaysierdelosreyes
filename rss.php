6c43ad9851722c4d54ad2b774cdd974edd58c4ff
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 22:24:45 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,7 +1,7 @@
 <?php
-    $rssf = '<?xml version="1.0" encoding="UTF-8"?>';
-    $rssf .= '<rss version="2.0">';
-    $rssf .= '<channel>';
+    $rss = '<?xml version="1.0" encoding="UTF-8"?>';
+    $rss .= '<rss version="2.0">';
+    $rss .= '<channel>';
 
     $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820410") or die (mysqli_error($con));
     $sql = "SELECT * FROM tblprofile";
@@ -10,16 +10,16 @@
     while($r= mysqli_fetch_assoc($q)){
         extract($r);
         
-        $rssf .= '<izone>';
-        $rssf .= '<Stage_Name>' . $Stage_Name . '</Stage_Name>';
-        $rssf .= '<Rank>' . $Rank . '</Rank>';
-        $rssf .= '<Birth_Name>' . $Birth_Name . '</Birth_Name>';
-        $rssf .= '<Birthday>' . $Birthday . '</Birthday>';
-        $rssf .= '<Position>' . $Position . '</Position>';
-        $rssf .= '</music>';
+        $rss .= '<izone>';
+        $rss .= '<Stage_Name>' . $Stage_Name . '</Stage_Name>';
+        $rss .= '<Rank>' . $Rank . '</Rank>';
+        $rss .= '<Birth_Name>' . $Birth_Name . '</Birth_Name>';
+        $rss .= '<Birthday>' . $Birthday . '</Birthday>';
+        $rssf.= '<Position>' . $Position . '</Position>';
+        $rss .= '</izone>';
     }
-    $rssf .= '</channel>';
-    $rssf .= '</rss>';
+    $rss .= '</channel>';
+    $rss .= '</rss>';
 
     echo $rss;
 ?>

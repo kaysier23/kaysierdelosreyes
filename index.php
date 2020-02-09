db200252836ea32874f0fc5b6ea8867646b25fd8
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-09 22:14:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 <?php
  foreach( $content as $data ){
      $mus_title = $data->getElementsByTagName("Stage_Name")->item(0)->nodeValue;
-     $mus_title = $data->getElementsByTagName("Birth_Name")->item(0)->nodeValue;
-     $mus_artist = $data->getElementsByTagName("Birthday")->item(0)->nodeValue;
-     $mus_genre = $data->getElementsByTagName("Rank")->item(0)->nodeValue;
+     $mus_title = $data->getElementsByTagName("Rank")->item(0)->nodeValue;
+     $mus_artist = $data->getElementsByTagName("Birth_Name")->item(0)->nodeValue;
+     $mus_genre = $data->getElementsByTagName("Birthday")->item(0)->nodeValue;
      $mus_writer = $data->getElementsByTagName("Position")->item(0)->nodeValue;
      echo "<ul>
             <h2>$Stage_Name</h2>
               <ul>
-                  <li>Birth_Name: $Birth_Name </li>
-                  <li>Birthday: $Birthday </li>
-                  <li>Rank:$Rank </li>
+                  <li>Birth_Name: $Rank</li>
+                  <li>Birthday: $Birth_Name </li>
+                  <li>Rank:$Birthday </li>
                   <li>Position: $Position</li>
               </ul>
           </ul>";
